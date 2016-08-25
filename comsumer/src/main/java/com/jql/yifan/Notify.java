ce036a1f3f4cfa5a1c8884a9a0eac9f537255ae2
金奇樑 <hzjinqiliang@corp.netease.com>
2016-08-25 14:15:25 +0800

    dubbo理论

--- comsumer/src/main/java/com/jql/yifan/Notify.java
+++ comsumer/src/main/java/com/jql/yifan/Notify.java
@@ -6,8 +6,17 @@ package com.jql.yifan;
  */
 public interface Notify {
 
+    /**
+     * onreturn 事件处理器
+     * @param result 方法调用返回的结果
+     */
     void onReturn(String result);
 
+    /**
+     * onthrow 事件处理器
+     * @param e 服务提供者抛出的异常
+     * @param messageId 消费者请求服务提供者的参数
+     */
     void onThrow(Throwable e, int messageId);
 
     void onInvoke(String result);
